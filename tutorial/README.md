2257610939d6f78097fc39e94bfa6f28820ed242
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2021-07-01 15:15:55 -0300

    Aula 7
    Adicionando Material App ao Widget

--- tutorial/README.md
+++ tutorial/README.md
@@ -5,3 +5,6 @@ Criando interface básica com estilo simples e sem uso de Stateless ou Statefull
 
 #### Aula 6
 O estado global é o Container que engloba tudo na aula 5. Os estados locais são componentes dentro dele, como: Center, Text, etc. Stateless widgets não são alterados após sua criação. Vemos criar classes com váriaveis e construtores para deixar o código desacoplado.
+
+#### Aula 7
+É possível utilizar um tema e estruturas predefinidas por plataforma, como por exemplo o MaterialApp para aplicações feitas para o público de Android e CupertinoApp para aplicações feitas para o público de iOS. É necessário adicionar um Widget pai e (no caso de Android é necessário um tema) uma home.
--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -11,14 +11,17 @@ class AppWidget extends StatelessWidget {
   const AppWidget({Key? key, required this.title}) : super(key: key);
 
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.green, fontSize: 18.0),
-        )
-      ),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container(
+        child: Center(
+          child: Text(
+            title,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.green, fontSize: 18.0),
+          )
+        ),
+      )
     );
   }
 }
